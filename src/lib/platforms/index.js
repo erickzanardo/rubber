d8e84ddde0c84611c71779d5023205e91d06f53b
Erick Zanardo <erickzanardoo@gmail.com>
2018-01-09 15:12:05 -0200

    Adding log

--- src/lib/platforms/index.js
+++ src/lib/platforms/index.js
@@ -1,10 +1,17 @@
 const is = platform => process.env.GAME_PLATFORM == platform
 
-export default {
+const Platforms = {
   isBrowser: () => is("browser"),
   isMobile: () => is("android") || is("ios"),
   isAndroid: () => is("android"),
   isIos: () => is("ios"),
 
-  isDebugging: () => !!process.env.DEBUG
+  isDebugging: () => !!process.env.DEBUG,
+  log: message => {
+    if (Platforms.isDebugging()) {
+      console.log(message)
+    }
+  }
 }
+
+export default Platforms
