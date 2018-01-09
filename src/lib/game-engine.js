b8d7e71f0907b75b1c2b203f790848b8e70f699a
Erick Zanardo <erickzanardoo@gmail.com>
2018-01-09 09:16:25 -0200

    Adding fps indicator

--- src/lib/game-engine.js
+++ src/lib/game-engine.js
@@ -1,7 +1,33 @@
 const now = () => new Date().getTime();
 
+const FPSIndicator = () => {
+  let time = 0;
+  let frames = 0;
+  let lastFrames = 0;
+
+  return {
+    countFrame: delta => {
+      time += delta;
+      frames++;
+      if (time >= 1000) {
+        time = 0;
+        lastFrames = frames;
+        frames = 0;
+      }
+    },
+    render: ctx => {
+      ctx.fillStyle = "#000e28";
+      ctx.fillRect(5, 5, 35, 25);
+
+      ctx.fillStyle = "#fff";
+      ctx.fillText(`${lastFrames} fps`, 10, 20);
+    }
+  }
+}
+
+
 const GameEngine = {
-  start: (canvas, gameLoop) => {
+  start: (canvas, gameLoop, { showFPS } = {}) => {
     console.log("Initializing Rubber");
 
     const width = document.body.clientWidth;
@@ -11,6 +37,8 @@ const GameEngine = {
     canvas.height = height;
 
     const graphics = canvas.getContext("2d");
+
+    const fpsIndicator = FPSIndicator()
 
     let time = now();
 
@@ -27,6 +55,11 @@ const GameEngine = {
 
       if (gameLoop.update) gameLoop.update(delta)
       if (gameLoop.render) gameLoop.render(graphics)
+
+      if (showFPS) {
+        fpsIndicator.countFrame(delta)
+        fpsIndicator.render(graphics)
+      }
 
       requestAnimationFrame(tick);
     }
